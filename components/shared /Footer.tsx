30f1f541f2f960e59242c21e7bea6d2cd3214e25
codewithfrancis <offor919@gmail.com>
2025-07-28 21:35:25 +0100

    feat: add footer shared component

---
+++ components/shared /Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <div className="text-center text-xs text-muted-foreground font-mono">
+        <p>&gt; Unauthorized access prohibited</p>
+        <p>&gt; All access attempts are logged</p>
+      </div>
+    );
+  }
+  
