064d2611c7e18ee9d6148371876fc90b729149d1
Dozie <offor919@gmail.com>
2025-09-27 22:36:36 +0100

    validate login

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -2,6 +2,9 @@ import { IsNotEmpty, MinLength } from 'class-validator';
 
 export class UserDto{
     @IsNotEmpty()
+    username: string;
+
+    @IsNotEmpty()
     @MinLength(8)
     password: string;
 } 
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -2,18 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { use } from 'react';
 @Injectable()
 export class UserService {
     constructor(private prisma: PrismaService) { }
-    async createUser(password: string) {
+    async createUser({username, password}: UserDto) {
         const hashpassword = await bcrypt.hash(password, 10);
         return this.prisma.user.create({
             data:{
+                username,
                password: hashpassword
             }
         })
     }
-    async validatePassword(password: UserDto ){
-        const user = await this.prisma.user.findFirst({where:{id: password.id}});
+    async validatePassword({username, password}: UserDto ){
+        const user = await this.prisma.user.findUnique({
+            where: {username}
+        });
+        if(user){
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if(isPasswordValid){
+                return user;
+            }
+            return null;
+        }
+        return null;
     }
 }
