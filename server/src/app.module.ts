c37a35810f2fc0575baef7e7f1335a8c36a93e69
Dozie <offor919@gmail.com>
2025-09-27 21:07:00 +0100

    db connetion secured

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
+
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+   UserModule,
+   PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
