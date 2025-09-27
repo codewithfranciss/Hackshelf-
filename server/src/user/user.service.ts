4b2640fbddaca5b0d221747244a9d6c9e5523b9d
Dozie <offor919@gmail.com>
2025-09-27 22:33:58 +0100

    add username column to user schema

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma: PrismaService) { }
+    async createUser(password: string) {
+        const hashpassword = await bcrypt.hash(password, 10);
+        return this.prisma.user.create({
+            data:{
+               password: hashpassword
+            }
+        })
+    }
+    async validatePassword(password: UserDto ){
+        const user = await this.prisma.user.findFirst({where:{id: password.id}});
+    }
+}
