48c9465ee1fc149bc04d375129ece8f2c1f9e6f7
Dozie <offor919@gmail.com>
2025-10-05 00:16:33 +0100

    add category controller

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,4 +1,23 @@
 import { Controller } from '@nestjs/common';
+import { Body, Post, Param, Get } from '@nestjs/common';
+import { CategoryService } from './category.service';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+    constructor(private categoryService: CategoryService) { }
+
+    @Post()
+    async createCategory(@Body() body: { name: string, description?: string }) {
+        return this.categoryService.createCategory(body);
+    }
+
+    @Get()
+    async getCategories() {
+        return this.categoryService.getCategories();
+    }
+
+    @Get(':id')
+    async getCategoryById(@Param('id') id: string) {
+        return this.categoryService.getCategoryById(Number(id));
+    }
+}
